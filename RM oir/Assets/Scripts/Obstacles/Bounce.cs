d5c84ff10d749fa2b1d8ce16c5282e5afdb74098
oirbeboq <revamqq@gmail.com>
2024-03-29 09:59:51 -0400

    ..

---
+++ RM oir/Assets/Scripts/Obstacles/Bounce.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float force = 10f; //Force 10000f
+    public float stunTime = 0.5f;
+    private Vector3 hitDir;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            if (collision.gameObject.tag == "Player")
+            {
+                hitDir = contact.normal;
+                collision.gameObject.GetComponent<PlayerMovement>().HitPlayer(-hitDir * force, stunTime);
+                return;
+            }
+        }
+
+    }
+}
--- RM oir/Assets/Scripts/PlayerController/PlayerMovement.cs
+++ RM oir/Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -50,6 +51,16 @@ public class PlayerMovement : MonoBehaviour
     //Sliding
     private Vector3 normalVector = Vector3.up;
     private Vector3 wallNormalVector;
+
+    private bool canMove = true; //If player is not hitted
+    private bool isStuned = false;
+    private bool wasStuned = false; //If player was stunned before get stunned another time
+    private float pushForce;
+    private Vector3 pushDir;
+
+    public Vector3 checkPoint;
+    private bool slide = false;
+    public float gravity = 10.0f;
 
     void Awake()
     {
@@ -297,5 +308,46 @@ public class PlayerMovement : MonoBehaviour
         grounded = false;
     }
 
+    public void HitPlayer(Vector3 velocityF, float time)
+    {
+        rb.velocity = velocityF;
+
+        pushForce = velocityF.magnitude;
+        pushDir = Vector3.Normalize(velocityF);
+        StartCoroutine(Decrease(velocityF.magnitude, time));
+    }
+
+    private IEnumerator Decrease(float value, float duration)
+    {
+        if (isStuned)
+            wasStuned = true;
+        isStuned = true;
+        canMove = false;
+
+        float delta = 0;
+        delta = value / duration;
+
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            yield return null;
+            if (!slide) //Reduce the force if the ground isnt slide
+            {
+                pushForce = pushForce - Time.deltaTime * delta;
+                pushForce = pushForce < 0 ? 0 : pushForce;
+                //Debug.Log(pushForce);
+            }
+            rb.AddForce(new Vector3(0, -gravity * GetComponent<Rigidbody>().mass, 0)); //Add gravity
+        }
+
+        if (wasStuned)
+        {
+            wasStuned = false;
+        }
+        else
+        {
+            isStuned = false;
+            canMove = true;
+        }
+    }
 }
 
