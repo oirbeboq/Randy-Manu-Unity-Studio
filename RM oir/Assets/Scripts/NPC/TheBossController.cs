7dfc2810aeb6b5a5a2b3e3f5a3fc2b1b9a050167
oirbeboq <revamqq@gmail.com>
2024-04-06 12:16:46 -0400

    .

---
+++ RM oir/Assets/Scripts/NPC/TheBossController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TheBossController : MonoBehaviour
+{
+    public NavMeshAgent theBossNav;
+
+    private void Update()
+    {
+        
+    }
+}
